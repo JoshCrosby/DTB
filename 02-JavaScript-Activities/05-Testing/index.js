2a96273bf295050be77e7c6b43f39d899078206f
JoshCrosby <joshdcrosby@gmail.com>
2019-12-09 13:50:01 -0600

    added some testing files

---
+++ 02-JavaScript-Activities/05-Testing/index.js
@@ -0,0 +1,20 @@
+var princesses = [{
+        name: "Repunzel",
+        age: 18
+    },
+    {
+        name: "Moana",
+        age: 16
+    },
+    {
+        name: "Elsa",
+        age: 16
+    },
+    {
+        name: "Brave",
+        age: 15
+    }
+];
+
+// Print princess name : age
+princesses.forEach((princess) => console.log(`${princess.name}: ${princess.age}`));
